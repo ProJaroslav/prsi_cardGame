a3e7c53eb0a46968ac1091a98981e349b672c0a1
Jaroslav Procházka <jaroslav.prossv@gmail.com>
2020-10-09 19:20:10 +0200

    vyprcanek

---
+++ script.js
@@ -0,0 +1,47 @@
+function Card(name, type, number) {
+    this.name = name
+    this.type = type,
+    this.number = number
+}
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+
+let cards = [];
+
+let deck = [];
+
+
+let hand = [];
+let player1_space = document.querySelector(".Player1")
+
+
+function createGame(){
+    let druhy = ["srdce", "zalud", "varlata", "zeleny"];
+    let typy = ["7", "8", "9", "10", "spodek", "vrsek", "kral"]
+    let cisla = [7, 8, 9, 10, 11, 12, 13]
+
+    for (let druh = 0; druh < 4; druh ++) {
+        for (let cislo = 0; cislo < 7; cislo ++) {
+            let new_card = new Card(druhy[druh], cisla[cislo], typy[cislo])
+            cards.push(new_card);
+        }
+    }
+}
+
+
+function createHand(){
+    shuffle(cards);
+    for (let counter = 0; counter < 4; counter ++) {
+        player1_space.textContent += cards[counter].name + "_" + cards[counter].number + " ";
+    }
+
+
+}
+
+//function play(){}
+createGame()
+createHand()
+console.log(cards)
